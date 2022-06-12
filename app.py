9d08525295b26019aab43b55e41a1badc36b2f54
zianttt <u2102755@siswa.um.edu.my>
2022-06-12 23:01:59 +0800

    Remove all questionnaire related part

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 from tweets import get_tweets_by_username
 from tweets_model import TweetModel
-from questions_model import QuestionModel
 import numpy as np
 
 app = Flask(__name__)
@@ -33,33 +32,6 @@ def dashboard():
 def results_by_username():
     username = request.form['username']
     return redirect(url_for('dashboard', username=username))
-
-
-@app.route('/resultsopt', methods=['POST'])
-def results_by_questions():
-    q1 = int(request.form['a1'])
-    q2 = int(request.form['a2'])
-    q3 = int(request.form['a3'])
-    q4 = int(request.form['a4'])
-    q5 = int(request.form['a5'])
-    q6 = int(request.form['a6'])
-    q7 = int(request.form['a7'])
-    q8 = int(request.form['a8'])
-    q9 = int(request.form['a9'])
-    sleep_hours = float(request.form['sleep_hours'])
-    gender = request.form['sleep_hours']
-    if gender.strip() == 'f':
-        new_gender = 0
-    else:
-        new_gender = 1
-    height = float(request.form['height'])
-    weight = float(request.form['weight'])
-    bmi = weight / (np.square(height))
-
-    values = [q1, q2, q3, q4, q5, q6, q7, q8, q9, sleep_hours, new_gender, bmi]
-    model = QuestionModel()
-    prediction = model.predict(np.array([values]))
-    return redirect(url_for('test', result=prediction))
 
 
 @app.route('/test')
